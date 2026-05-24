604f72f83bb99b9375445db4a3b4c22018ed42bb
nikolaj <nikolajulstrup@outlook.com>
2026-05-25 00:43:40 +0200

    added pipe spawner

--- flappy-bird-v-2/Scenes/pipe_behaviour.gd
+++ flappy-bird-v-2/Scenes/pipe_behaviour.gd
@@ -4,16 +4,9 @@ extends Node2D
 @export var pipe_speed : int = 500
 var delete_distance : int = -1000
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	reset()
-
-func reset() -> void:
-	position = start_pos
-
 func _process(delta: float) -> void:
 	if Global.game_running == true:
-		move()
+		move(delta)
 
-func move() -> void:
-	position.x -= pipe_speed
+func move(delta: float) -> void:
+	position.x -= pipe_speed * delta
--- flappy-bird-v-2/Scripts/game_manager.gd
+++ flappy-bird-v-2/Scripts/game_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+@export var pipe_spawner : Timer
+var pipe_scene = preload("res://Scenes/pipes.tscn")
+
 func _ready() -> void:
 	reset()
 
@@ -7,8 +10,21 @@ func reset():
 	Global.game_running = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
+	# Tjek om spil skal startes
+	if Input.is_action_just_pressed("jump") and Global.game_running == false:
 		Global.game_running = true
-	if Global.game_running == true:
+		pipe_spawner.start()
+	
+	# Når spil er startet
+	if Global.game_running == true: # Begynd at spawn pipes
 		pass
-		# call PipeSpawner Timer
+		
+	#print(Global.game_running)
+
+# Genererer pipes
+func _on_pipe_spawner_timeout() -> void:
+	print("pipe created")
+	var pipe_instance = pipe_scene.instantiate()
+	pipe_instance.position = Vector2(1500, randi_range(-400, 250))
+	add_child(pipe_instance)
+	
