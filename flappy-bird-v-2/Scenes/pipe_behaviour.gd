fc97b49dc5d2945d3665c9afe50d80d83fbc747a
nikolaj <nikolajulstrup@outlook.com>
2026-05-25 12:35:05 +0200

    added difficulty via pipe gap size

--- flappy-bird-v-2/Scenes/pipe_behaviour.gd
+++ flappy-bird-v-2/Scenes/pipe_behaviour.gd
@@ -7,6 +7,11 @@ var delete_distance : int = -1000
 func _process(delta: float) -> void:
 	if Global.game_running == true:
 		move(delta)
+		delete()
 
 func move(delta: float) -> void:
 	position.x -= pipe_speed * delta
+
+func delete():
+	if position.x <= delete_distance:
+		queue_free()
--- flappy-bird-v-2/Scripts/game_manager.gd
+++ flappy-bird-v-2/Scripts/game_manager.gd
@@ -2,29 +2,34 @@ extends Node
 
 @export var pipe_spawner : Timer
 var pipe_scene = preload("res://Scenes/pipes.tscn")
+const max_pipe_gap_size : int = 100
+const min_pipe_gap_size : int = -100
+var current_pipe_gap_size : int
+const pipe_gap_decrement : int = 5
 
 func _ready() -> void:
 	reset()
 
 func reset():
 	Global.game_running = false
+	current_pipe_gap_size = max_pipe_gap_size 
 
 func _process(delta: float) -> void:
 	# Tjek om spil skal startes
 	if Input.is_action_just_pressed("jump") and Global.game_running == false:
 		Global.game_running = true
 		pipe_spawner.start()
-	
-	# Når spil er startet
-	if Global.game_running == true: # Begynd at spawn pipes
-		pass
-		
-	#print(Global.game_running)
 
 # Genererer pipes
 func _on_pipe_spawner_timeout() -> void:
-	print("pipe created")
 	var pipe_instance = pipe_scene.instantiate()
-	pipe_instance.position = Vector2(1500, randi_range(-400, 250))
-	add_child(pipe_instance)
 	
+	pipe_instance.position = Vector2(1500, randi_range(-400, 200))
+	pipe_instance.get_child(0).position.y -= randi_range(min_pipe_gap_size, current_pipe_gap_size)
+	pipe_instance.get_child(1).position.y += randi_range(min_pipe_gap_size, current_pipe_gap_size)
+	if current_pipe_gap_size <= min_pipe_gap_size:
+		current_pipe_gap_size = min_pipe_gap_size
+	else: 
+		current_pipe_gap_size -= pipe_gap_decrement
+	print(current_pipe_gap_size)
+	add_child(pipe_instance)
